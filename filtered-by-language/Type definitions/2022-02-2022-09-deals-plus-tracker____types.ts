03bedf3bc53b0a36c081c61af9f77221b1bf8541
Martin Burford <martin@martinburford.co.uk>
2024-06-23 16:29:58 +0100

    Removal of filtered by language grouping

--- filtered-by-language/Type definitions/2022-02-2022-09-deals-plus-tracker____types.ts
+++
@@ -1,5 +0,0 @@
-export type TOptional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
-
-export type TPickEnum<T, K extends T> = {
-  [P in keyof K]: P extends K ? P : never;
-};
--- filtered-by-language/Type definitions/2023-09-2023-12-personal-portfolio-portfolio____responsive.types.ts
+++
@@ -1,6 +0,0 @@
-// Type imports
-import { EBreakpoints } from "types/enums";
-
-// Types
-export type TBreakpointRender = ({ children }: { children: React.ReactElement }) => React.ReactElement | null;
-export type TCurrentBreakpoint = () => EBreakpoints;
